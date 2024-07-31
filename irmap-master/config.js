487a25a21077ebbc5443f2b990481abf2c6db85e
IGZK <142678684+IGZK@users.noreply.github.com>
2024-07-31 17:48:56 +0800

    Add files via upload

---
+++ irmap-master/config.js
@@ -0,0 +1,27 @@
+/** 更多样式 http://developer.baidu.com/map/custom/list.htm */
+var MAP_STYLE = "hardedge";
+var DATA = {
+	"安徽省": {
+		"安徽医科大学": ["蔡振宇"],
+		"安徽中医药大学": ["胡可继"]
+	},
+
+	"南京": {
+		"南京航空航天大学": ["高文锴", "吕天麟"]
+	},
+	
+	"北京": {
+		"北京航空航天大学": ["何明轩"]
+	}
+};
+
+var SPEC_POS = {
+	"univ_C" : [121.597479, 31.185356]
+};
+
+var MAP_TITLE = "毕业去向";
+
+var ABOUT = {
+	"作者" : ["Yuehao"],
+	"框架" : ["<a href='https://github.com/yuehaowang/irmap' target='_blank'>irmap</a>", "Bootstrap", "百度地图"]
+};
